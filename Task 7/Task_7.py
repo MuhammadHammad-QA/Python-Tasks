048f48b3b8b20c62b0e3df729b3aa19788f1b90b
Hammad <hammad@emumba.com>
2024-06-25 16:45:48 +0500

    Add task7 and txt file.In this task read from txt file and print unique words plus their frequency

---
+++ Task 7/Task_7.py
@@ -0,0 +1,23 @@
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().strip().split()
+    
+    n = int(data[0])
+    words = data[1:]
+    
+    from collections import OrderedDict
+
+    word_count = OrderedDict()
+    
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    print(len(word_count))
+    print(" ".join(map(str, word_count.values())))
+
+if __name__ == "__main__":
+    main()
